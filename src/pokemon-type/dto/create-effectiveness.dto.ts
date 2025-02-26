1387af1229a06a6c9f54b85c9cd9e2d9c165a554
KennyKeni <linkenny917@gmail.com>
2025-02-26 12:27:44 -0500

    both effectiveness

--- src/pokemon-type/dto/create-effectiveness.dto.ts
+++ src/pokemon-type/dto/create-effectiveness.dto.ts
@@ -1,14 +1,36 @@
 import { IsNotEmpty, IsNumber, Max, Min } from "class-validator";
 
-export class CreateEffectivenessAttackDto {
-  // This is not needed because of @Param
-  // @IsNotEmpty()
-  // @IsNumber()
-  // attacking_type!: number;
+export class CreateEffectivenessDto {
+  @IsNotEmpty()
+  @IsNumber()
+  attacking_type!: number;
 
   @IsNotEmpty()
   @IsNumber()
   defending_type!: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  @Max(4)
+  multiplier!: number;
+}
+export class CreateEffectivenessAttackDto {
+  @IsNotEmpty()
+  @IsNumber()
+  defending_type!: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  @Max(4)
+  multiplier!: number;
+}
+
+export class CreateEffectivenessDefenseDto {
+  @IsNotEmpty()
+  @IsNumber()
+  attacking_type!: number;
 
   @IsNotEmpty()
   @IsNumber()
--- src/pokemon-type/pokemon-type.service.ts
+++ src/pokemon-type/pokemon-type.service.ts
@@ -2,7 +2,7 @@ import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityManager, EntityRepository, UniqueConstraintViolationException } from '@mikro-orm/postgresql';
 import { ConflictException, Injectable } from '@nestjs/common';
 import { Effectiveness, PokemonType } from './entities';
-import { CreateEffectivenessAttackDto, CreatePokemonTypeDto } from './dto';
+import { CreateEffectivenessAttackDto, CreateEffectivenessDefenseDto, CreatePokemonTypeDto } from './dto';
 
 @Injectable()
 export class PokemonTypeService {
@@ -12,6 +12,8 @@ export class PokemonTypeService {
     private readonly em: EntityManager,
   ) {}
 
+  // I should make another "create" that makes default relations, so that we will always guarantee N:M relation
+  // between all types (default mult. of 1)
   async create(createPokemonTypeDto: CreatePokemonTypeDto) {
     try {
       const pokemonType = this.em.create(PokemonType, createPokemonTypeDto);
@@ -26,15 +28,25 @@ export class PokemonTypeService {
     }
   }
 
-  async createEffectivenessAttack(id: string, createAttackEffectiveness: CreateEffectivenessAttackDto) {
+  async createEffectivenessAttack(id: string, createEffectivenessAttackDto: CreateEffectivenessAttackDto) {
+    const attacking_type = this.pokemonTypeRepository.getReference(+id);
+    const defending_type = this.pokemonTypeRepository.getReference(createEffectivenessAttackDto.defending_type);
+    return await this.createEffectiveness(attacking_type, defending_type, createEffectivenessAttackDto.multiplier);
+  }
+
+  async createEffectivenessDefense(id: string, createEffectivenessDefenseDto: CreateEffectivenessDefenseDto) {
+    const attacking_type = this.pokemonTypeRepository.getReference(createEffectivenessDefenseDto.attacking_type);
+    const defending_type = this.pokemonTypeRepository.getReference(+id);
+    return await this.createEffectiveness(attacking_type, defending_type, createEffectivenessDefenseDto.multiplier);
+  }
+
+  private async createEffectiveness(attacking_type: PokemonType, defending_type: PokemonType, multiplier: number) {
     try {
-      const attacking_type = this.pokemonTypeRepository.getReference(+id);
-      const defending_type = this.pokemonTypeRepository.getReference(createAttackEffectiveness.defending_type);
-      const multiplier = createAttackEffectiveness.multiplier;
 
       const effectivenes = this.em.create(Effectiveness, {attacking_type, defending_type, multiplier});
       await this.em.persistAndFlush(effectivenes);
       return effectivenes;
+
     } catch (error) {
       if (error instanceof UniqueConstraintViolationException) {
         throw new ConflictException(`dupe`);
